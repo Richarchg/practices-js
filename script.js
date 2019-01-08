84fe3e6ed516b7abe7ed234b9b6b4aeb3f6b2a84
Richar <richarchinchillagomez@gmail.com>
2019-01-08 13:03:23 -0500

    se modificó el js pero se mejora mi conocimiento

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-let text = "Bienvenidos a juegos mentales";
+let name = "richar", cuestion;
 
 
-// console.log(text.substring(8,11))
-// console.log(text.substr(8,3))
-console.log(text.slice(8,-5))
-
-
-// console.clear()
-
+do {
+    cuestion = prompt("what is my name?");
+} while (cuestion.toLocaleLowerCase() != name);
